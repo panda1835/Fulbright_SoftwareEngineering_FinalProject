38dd67627399c1fa929da335952498971357495e
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-29 10:23:25 +0700

    inti UserRepo

---
+++ course-registration/src/main/java/com/se2020/course/registration/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.se2020.course.registration.repository;
+
+import com.se2020.course.registration.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
