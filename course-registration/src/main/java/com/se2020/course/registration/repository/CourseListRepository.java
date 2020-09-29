dc4df8b78dbd1d574b023cb676c67f8f8b052e78
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-29 10:22:41 +0700

    Change CourseListRepo -> CourseRepo

--- course-registration/src/main/java/com/se2020/course/registration/repository/CourseListRepository.java
+++
@@ -1,5 +0,0 @@
-package com.se2020.course.registration.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseListRepository extends JpaRepository<> {
-}
