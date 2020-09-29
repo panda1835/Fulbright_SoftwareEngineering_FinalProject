7cbc7b6e69e6ac4a59ba24a996f1283a07fe27d9
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-29 10:50:16 +0700

    modify user.java

--- course-registration/src/main/java/com/se2020/course/registration/entity/User.java
+++ course-registration/src/main/java/com/se2020/course/registration/entity/User.java
@@ -1,8 +1,21 @@
 package com.se2020.course.registration.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue
 
+    private String userName;
+    private String email;
+    private String hashedPassword;
+    private String role;
+    
+    
 }
