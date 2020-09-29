b051f6ee32eb741eb581da6737784dc0c1ad5889
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-29 10:23:03 +0700

    init CourseRepo

---
+++ course-registration/src/main/java/com/se2020/course/registration/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.se2020.course.registration.repository;
+
+import com.se2020.course.registration.entity.Student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+    
+}
