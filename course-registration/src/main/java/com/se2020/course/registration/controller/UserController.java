605ce8bbaae43a373fef608d1cb5b5209b4b95f4
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-29 10:50:01 +0700

    add student: browse course

--- course-registration/src/main/java/com/se2020/course/registration/controller/UserController.java
+++ course-registration/src/main/java/com/se2020/course/registration/controller/UserController.java
@@ -1,13 +1,37 @@
 package com.se2020.course.registration.controller;
 
-import com.se2020.course.registration.repository.CourseListRepository;
+import java.util.*;
+
+import com.se2020.course.registration.entity.Course;
+import com.se2020.course.registration.entity.User;
+import com.se2020.course.registration.repository.CourseRepository;
+import com.se2020.course.registration.repository.StudentRepository;
+import com.se2020.course.registration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Autowired
-    private CourseListRepository courseListRepository;
+    private CourseRepository courseRepository;
+    private StudentRepository studentRepository;
+    private UserRepository userRepository;
 
-    //METHODS:
+    //ALL USERS
+    @GetMapping("/browse")
+    public List<Course> browseCourse(){
+        return courseRepository.findAll();
+    }
+
+    // ADMIN
+
+
+    // STUDENT
+    @PutMapping("/cancel/{courseId}")
+    public String cancelCourse(){
+        
+    }
 }
