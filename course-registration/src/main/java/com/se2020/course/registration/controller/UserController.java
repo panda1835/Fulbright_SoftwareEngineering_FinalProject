5bdd913b82cf168d27169fcde252086a8fcc3f93
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-09-30 14:51:54 +0700

    Add student register for course

--- course-registration/src/main/java/com/se2020/course/registration/controller/UserController.java
+++ course-registration/src/main/java/com/se2020/course/registration/controller/UserController.java
@@ -3,6 +3,7 @@ package com.se2020.course.registration.controller;
 import java.util.*;
 
 import com.se2020.course.registration.entity.Course;
+import com.se2020.course.registration.entity.Student;
 import com.se2020.course.registration.entity.User;
 import com.se2020.course.registration.repository.CourseRepository;
 import com.se2020.course.registration.repository.StudentRepository;
@@ -30,8 +31,46 @@ public class UserController {
 
 
     // STUDENT
-    @PutMapping("/cancel/{courseId}")
-    public String cancelCourse(){
+    /**
+     * Register for a course
+     */
+    @PutMapping("/register/{courseId}")
+    public String cancelCourse(@RequestBody Student student, @PathVariable String courseId){
+        Course course = courseRepository.findById(courseId);
+        String studentId = student.getStudentId();
         
+        // check capacity
+        if (course.getCurrentCapacity() < course.getCapacity()){
+            return "This course is already full";
+        }
+
+        // check student register status
+        for (String id: course.getStudentList()){
+            if (id.compareTo(studentId) == 0){
+                return "You already registered for this course";
+            }
+        }
+
+        // check prerequisite
+        List<String> prerequisite = course.getPrerequisite();
+        List<String> pastCourse = student.getPastCourses();
+        for (String pre: prerequisite){
+            for (int i = 0; i < pastCourse.size(); i++){
+                if (pastCourse.get(i).compareTo(pre) == 0) {
+                    continue;
+                }
+            }
+            return "You are not fulfill the prerequisite";
+        }
+
+        // success, update database
+        Student updateStudent = studentRepository.getOne(studentId);
+        Student updateCourse = courseRepository.getOne(courseId);
+        updateStudent.getCurrentRegisteredCourse().add(courseId);
+        updateCourse.getStudentList().add(studentId);
+        studentRepository.save(updateStudent);
+        courseRepository.save(updateCourse);
+
+        return "You successfully register for this course";
     }
 }
