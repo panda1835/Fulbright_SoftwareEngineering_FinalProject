e158d8f57c7d73cde50881cf9a7dc2b957d2f463
panda1835 <phuc.le@ug.fuv.edu.vn>
2020-10-01 13:37:51 +0700

    fix import error

--- course-registration/src/main/java/com/se2020/course/registration/repository/CourseRepository.java
+++ course-registration/src/main/java/com/se2020/course/registration/repository/CourseRepository.java
@@ -3,7 +3,9 @@ package com.se2020.course.registration.repository;
 import com.se2020.course.registration.entity.Course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.*;
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    List<User> findByCourseID(@Param("courseId") String courseId);
+    List<Course> findByCourseID(@Param("courseId") String courseId);
 }
--- course-registration/src/main/java/com/se2020/course/registration/repository/UserRepository.java
+++ course-registration/src/main/java/com/se2020/course/registration/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.se2020.course.registration.repository;
 import com.se2020.course.registration.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.*;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     List<User> findByUserID(@Param("userId") String userId);
