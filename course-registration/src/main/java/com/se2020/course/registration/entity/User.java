f804b7c998ee9588056665511f8f874ff4404c3f
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-10-04 17:43:48 +0700

    Use @Data from Lombok to replace getters and setters code

--- course-registration/src/main/java/com/se2020/course/registration/entity/User.java
+++ course-registration/src/main/java/com/se2020/course/registration/entity/User.java
@@ -1,67 +1,24 @@
 package com.se2020.course.registration.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
 @Entity
+@Data
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String userName;
-    private String email;
-    private String hashedPassword;
-    private String role;
     private String userId;
+    private String email;
+    private String password;
+    private String role;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getHashedPassword() {
-        return hashedPassword;
-    }
-
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 }
