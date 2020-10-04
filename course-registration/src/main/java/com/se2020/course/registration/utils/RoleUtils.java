e1c74c1f7dfe9aa27bc4dbfd1dec78ef6f6e7b44
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-10-05 02:37:09 +0700

    Add spacing to follow Java convention

--- course-registration/src/main/java/com/se2020/course/registration/utils/RoleUtils.java
+++ course-registration/src/main/java/com/se2020/course/registration/utils/RoleUtils.java
@@ -13,14 +13,16 @@ import static com.se2020.course.registration.enums.RolesEnum.ADMIN;
 import static com.se2020.course.registration.enums.RolesEnum.STUDENT;
 
 public class RoleUtils {
-    public static final List<PermissionsEnum> ADMIN_PERMISSION = new ArrayList<>(Arrays.asList(PermissionsEnum.values()));
+    public static final List<PermissionsEnum> ADMIN_PERMISSION = new ArrayList<>(
+            Arrays.asList(PermissionsEnum.values()));
 
     public static final List<PermissionsEnum> STUDENT_PERMISSION = new ArrayList<>(Arrays.asList(
             PermissionsEnum.LOG_IN,PermissionsEnum.CHANGE_PASSWORD,PermissionsEnum.MODIFY_PROFILE,
             PermissionsEnum.REGISTER_COURSE, PermissionsEnum.CANCEL_COURSE, PermissionsEnum.GET_COURSE,
             PermissionsEnum.GET_MY_COURSE));
 
-    public static final List<PermissionsEnum> GUEST_PERMISSION = new ArrayList<>(Collections.singletonList(PermissionsEnum.GET_COURSE));
+    public static final List<PermissionsEnum> GUEST_PERMISSION = new ArrayList<>(
+            Collections.singletonList(PermissionsEnum.GET_COURSE));
 
     public static List<PermissionsEnum> getPermissionFromRole(RolesEnum role){
         List<PermissionsEnum> permissions = null;
