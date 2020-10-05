d0528c3346aef6e3f72d5e910e6925c73c278408
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-10-06 04:51:23 +0700

    Remove redundant uncommented code

--- course-registration/src/main/java/com/se2020/course/registration/entity/Student.java
+++ course-registration/src/main/java/com/se2020/course/registration/entity/Student.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import com.se2020.course.registration.entity.Course;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import lombok.Data;
 
@@ -21,97 +19,16 @@ public class Student{
     private String studentId;
     private String email;
 
-//    private String dob;
-//    private int gradYear;
-//    private String aboutMe;
-//
-//    private int numCredits;
+    private String dob;
+    private int gradYear;
+    private String aboutMe;
+
+    private int numCredits;
+
 //    private List<String> pastCourses; // courseId
 //    private List<String> currentRegisteredCourse; // courseId
 //
-//    Student(){}
+    Student(){}
 //
-//    Student(String studentId){
-//        this.studentId = studentId;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getStudentId() {
-//        return studentId;
-//    }
-//
-//    public void setStudentId(String studentId) {
-//        this.studentId = studentId;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getDob() {
-//        return dob;
-//    }
-//
-//    public void setDob(String dob) {
-//        this.dob = dob;
-//    }
-//
-//    public int getGradYear() {
-//        return gradYear;
-//    }
-//
-//    public void setGradYear(int gradYear) {
-//        this.gradYear = gradYear;
-//    }
-//
-//    public String getAboutMe() {
-//        return aboutMe;
-//    }
-//
-//    public void setAboutMe(String aboutMe) {
-//        this.aboutMe = aboutMe;
-//    }
-//
-//    public int getNumCredits() {
-//        return numCredits;
-//    }
-//
-//    public void setNumCredits(int numCredits) {
-//        this.numCredits = numCredits;
-//    }
-//
-//    public List<String> getPastCourses() {
-//        return pastCourses;
-//    }
-//
-//    public void setPastCourses(List<String> pastCourses) {
-//        this.pastCourses = pastCourses;
-//    }
-//
-//    public List<String> getCurrentRegisteredCourse() {
-//        return currentRegisteredCourse;
-//    }
-//
-//    public void setCurrentRegisteredCourse(List<String> currentRegisteredCourse) {
-//        this.currentRegisteredCourse = currentRegisteredCourse;
-//    }
+
 }
