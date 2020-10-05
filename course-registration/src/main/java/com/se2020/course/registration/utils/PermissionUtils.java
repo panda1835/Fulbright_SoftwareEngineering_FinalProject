08ff797225e8006ae503c257dcce6d6b9a53fce6
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-10-05 19:40:44 +0700

    Replace parameter RolesEnum role (deleted enum) with String role

--- course-registration/src/main/java/com/se2020/course/registration/utils/PermissionUtils.java
+++ course-registration/src/main/java/com/se2020/course/registration/utils/PermissionUtils.java
@@ -2,7 +2,6 @@ package com.se2020.course.registration.utils;
 
 import com.se2020.course.registration.entity.User;
 import com.se2020.course.registration.enums.PermissionsEnum;
-import com.se2020.course.registration.enums.RolesEnum;
 import com.se2020.course.registration.repository.UserRepository;
 
 import java.util.List;
@@ -16,7 +15,7 @@ public class PermissionUtils {
         if (requesters.size() > 0) {
             User requester = requesters.get(0);
             String role = requester.getRole().toUpperCase();
-            List<PermissionsEnum> permissionFromRole = RoleUtils.getPermissionFromRole(RolesEnum.valueOf(role));
+            List<PermissionsEnum> permissionFromRole = RoleUtils.getPermissionFromRole(role);
             return permissionFromRole.contains(permission);
         }
         return false;
