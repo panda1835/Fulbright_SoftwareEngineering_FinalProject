4dca3f8521857c29c6511a98bee45a7b2e80f09c
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-10-05 14:49:48 +0700

    Add parameter permission to specify the permission type

--- course-registration/src/main/java/com/se2020/course/registration/utils/PermissionUtils.java
+++ course-registration/src/main/java/com/se2020/course/registration/utils/PermissionUtils.java
@@ -1,19 +1,23 @@
 package com.se2020.course.registration.utils;
 
 import com.se2020.course.registration.entity.User;
+import com.se2020.course.registration.enums.PermissionsEnum;
 import com.se2020.course.registration.enums.RolesEnum;
 import com.se2020.course.registration.repository.UserRepository;
 
 import java.util.List;
 
 public class PermissionUtils {
-    public static boolean hasPermission(String email, String password, UserRepository userRepository) {
+    public static boolean hasPermission(PermissionsEnum permission,
+                                        String email, String password,
+                                        UserRepository userRepository) {
         String hashed = SecurityUtils.hashPassword(password);
         List<User> requesters = userRepository.findByEmailAndPassword(email, hashed);
         if (requesters.size() > 0) {
             User requester = requesters.get(0);
             String role = requester.getRole().toUpperCase();
-            return RolesEnum.valueOf(role).equals(RolesEnum.ADMIN);
+            List<PermissionsEnum> permissionFromRole = RoleUtils.getPermissionFromRole(RolesEnum.valueOf(role));
+            return permissionFromRole.contains(permission);
         }
         return false;
     }
